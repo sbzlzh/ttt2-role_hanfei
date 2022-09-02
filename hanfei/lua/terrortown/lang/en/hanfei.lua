0e9ffe87f7b49b16979f3fd97c3b14f41847172a
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 01:35:22 +0800

    Add files via upload

---
+++ hanfei/lua/terrortown/lang/en/hanfei.lua
@@ -0,0 +1,12 @@
+local L = LANG.GetLanguageTableReference("en")
+
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HANFEI.name] = "Middle East Gangster"
+L["info_popup_" .. HANFEI.name] = [[You are a middle east gangster!
+Don't be shy, just do it!]]
+L["body_found_" .. HANFEI.abbr] = "This is the Middle East gangster."
+L["search_role_" .. HANFEI.abbr] = "This guy is a Middle Eastern gangster!"
+L["target_" .. HANFEI.name] = "Middle East Gangster"
+L["ttt2_desc_" .. HANFEI.name] = [[This is an even stronger traitor.
+Start with 2C4, armor, AK, can trigger Ulala after death!]]
