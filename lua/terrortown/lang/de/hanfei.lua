4976594c70eecdd63bf1788a13e64d9db664fce3
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 16:05:02 +0800

    Add files via upload

---
+++ lua/terrortown/lang/de/hanfei.lua
@@ -0,0 +1,12 @@
+local L = LANG.GetLanguageTableReference("de")
+
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HANFEI.name] = "Gangster aus dem Nahen Osten"
+L["info_popup_" .. HANFEI.name] = [[Du bist ein Gangster aus dem Nahen Osten!
+Sei nicht schüchtern, mach es einfach!]]
+L["body_found_" .. HANFEI.abbr] = "Das ist der Gangster aus dem Nahen Osten."
+L["search_role_" .. HANFEI.abbr] = "Dieser Typ ist ein Gangster aus dem Nahen Osten!"
+L["target_" .. HANFEI.name] = "Gangster aus dem Nahen Osten"
+L["ttt2_desc_" .. HANFEI.name] = [[Dies ist ein noch stärkerer Verräter.
+Beginnen Sie mit 2C4, Rüstung, AK, kann Ulala nach dem Tod auslösen!]]
