6cd6d7c5eb1ca0e4625b40957340c28fb3c79e3c
波と一緒に漂う <2247564152@qq.com>
2023-04-14 00:59:29 +0800

    Optimize code to add new features
    Change: When on the same team, the explosion does not deal damage; otherwise, it causes damage.

--- lua/terrortown/entities/roles/hanfei/shared.lua
+++ lua/terrortown/entities/roles/hanfei/shared.lua
@@ -1,8 +1,7 @@
 if SERVER then
 	AddCSLuaFile()
+
 	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_hanf.vmt")
-	resource.AddFile("materials/vgui/ttt/ak47_icon.vmt")
-	resource.AddFile("sound/weapons/hanfei/jihad.wav")
 end
 
 function ROLE:PreInitialize()
@@ -14,11 +13,12 @@ function ROLE:PreInitialize()
 	self.score.killsMultiplier = 2
 	self.score.teamKillsMultiplier = -16
 	self.score.bodyFoundMuliplier = 0
-	
+
 	self.defaultTeam = TEAM_TRAITOR
 	self.defaultEquipment = SPECIAL_EQUIPMENT
+
 	self.preventFindCredits = true
-	
+
 	self.conVarData = {
 		pct = 0.13,
 		maximum = 1,
@@ -28,105 +28,159 @@ function ROLE:PreInitialize()
 		togglable = true,
 		credits = 2,
 		creditsAwardDeadEnable = 1,
-		creditsAwardKillEnable = 1,
-		shopFallback = nil
+		creditsAwardKillEnable = 1
+		--shopFallback = nil
 	}
 end
-
 
 function ROLE:Initialize()
 	roles.SetBaseRole(self, ROLE_TRAITOR)
 end
 
--- explosion properties
-local function hanfei_Explode(ply,pos)
-	local dmg = 350
-	local r_inner = 500
-	local r_outer = r_inner * 1.2
- 	util.BlastDamage( ply, ply, pos, r_outer,dmg )
+-- hanfei_Explode function creates an explosion that deals damage within a specified range
+local function hanfei_Explode(ply, pos)
+	local DMG_EXPLOSION = 350
+	local R_INNER = 480
+	local R_OUTER = R_INNER * 1.2
+
+	-- Create and set explosion effect data
 	local effect = EffectData()
 	effect:SetStart(pos)
 	effect:SetOrigin(pos)
-	effect:SetScale(r_outer)
-	effect:SetRadius(r_outer)
-	effect:SetMagnitude(dmg)
+	effect:SetScale(R_OUTER)
+	effect:SetRadius(R_OUTER)
+	effect:SetMagnitude(DMG_EXPLOSION)
+
+	-- Generate explosion effect at the specified position
 	util.Effect("Explosion", effect, true, true)
+
+	-- Check all entities within the range
+	local entities = ents.FindInSphere(pos, R_OUTER)
+	for _, ent in ipairs(entities) do
+		-- If the entity is a player and not on the same team as the player who triggered the explosion
+		if ent:IsPlayer() and ent:GetTeam() ~= ply:GetTeam() then
+			-- Calculate distance and damage
+			local distance = pos:Distance(ent:GetPos())
+			local damage = DMG_EXPLOSION * (1 - (distance / R_OUTER))
+
+			-- Deal damage to the player
+			local dmginfo = DamageInfo()
+			dmginfo:SetDamage(damage)
+			dmginfo:SetAttacker(ply)
+			dmginfo:SetInflictor(ply)
+			dmginfo:SetDamageType(DMG_BLAST)
+			ent:TakeDamageInfo(dmginfo)
+		end
+	end
 end
+
 if SERVER then
-    -- adding loadout on role change/spawn
+	-- adding loadout on role change/spawn
 	function ROLE:GiveRoleLoadout(ply, isRoleChange)
-		ply:GiveEquipmentWeapon("weapon_ttt_ak57")
-		ply:GiveAmmo(90,"SMG1")
-		ply:GiveEquipmentWeapon("weapon_ttt_c4")
-		--ply:Give("weapon_ttt_kraber")
+		-- Assign equipment and weapons to the player
+		-- If player doesn't have AK57, give AK57
+		if not ply:HasWeapon("weapon_ttt_ak57") then
+			ply:GiveEquipmentWeapon("weapon_ttt_ak57")
+		end
+
+		-- If player does not have C4, give C4
+		if not ply:HasWeapon("weapon_ttt_c4") then
+			ply:GiveEquipmentWeapon("weapon_ttt_c4")
+		end
+
+		-- Give the player extra ammo
+		ply:GiveAmmo(90, "SMG1")
+
+		-- Give the player radar equipment
 		ply:GiveEquipmentItem("item_ttt_radar")
+
+		-- Give the player armor and set the life value
 		ply:GiveArmor(GetConVar("ttt_hanfei_armor"):GetInt())
-	    ply:SetHealth(GetConVar("ttt_hanfei_hp"):GetInt())
+		ply:SetHealth(GetConVar("ttt_hanfei_hp"):GetInt())
 	end
 
-	hook.Add( "PlayerDeath", "ttt_hanfei_death", function( victim, inflictor, attacker )
-		if victim:GetSubRole()==ROLE_HANFEI then
-			local pos=victim:GetPos()
+	hook.Add("PlayerDeath", "ttt_hanfei_death", function(victim, inflictor, attacker)
+		if victim:GetSubRole() == ROLE_HANFEI then
+			local pos = victim:GetPos()
 			timer.Simple(2.05, function()
-				hanfei_Explode(victim,pos) 
+				hanfei_Explode(victim, pos)
 			end)
 			victim:EmitSound("weapons/hanfei/jihad.wav", math.random(100, 150), math.random(95, 105))
 		end
 	end)
-	
-	hook.Add("TTTBeginRound","ttt_hanfei_timer",function()
-	    timer.Simple(GetConVar("ttt_hanfei_exposetime"):GetInt(), function()
-		    local flag=false
-		    for k,v in ipairs(player.GetAll()) do
-			if v:GetSubRole()==ROLE_HANFEI then
-				flag=true
-				v.expose=true
-				--v:SetModel("models/cso2/pm/hasanpm.mdl")
-			end
-		end
-		    if flag then
-			    for k,v in ipairs(player.GetAll()) do
-				    if v:GetSubRole()==ROLE_HANFEI then
-			            info=LANG.MsgAll("ttt2_hanfei_chat_reveal",{playername = v:Nick()}, MSG_CHAT_WARN)
-				    end
+
+	-- Set the exposure time for the hanfei role at the start of each round
+	hook.Add("TTTBeginRound", "ttt_hanfei_timer", function()
+		-- Get the exposure time from ConVar
+		timer.Simple(GetConVar("ttt_hanfei_exposetime"):GetInt(), function()
+			local flag = false
+			local hanfei_players = ""
+
+			-- check if there is a Hanfei character in the game
+			for k, v in ipairs(player.GetAll()) do
+				if v:GetSubRole() == ROLE_HANFEI then
+					flag = true
+					v.expose = true
+					--v:SetModel("models/cso2/pm/hasanpm.mdl")
+					hanfei_players = hanfei_players .. v:Nick() .. ", "
 				end
-		    end
-	    end)
-    end)
-    -- removing loadout on role change/despawn
+			end
+
+			--if there is a Hanfei character, notify all players
+			if flag then
+				hanfei_players = string.sub(hanfei_players, 1, -3) -- remove the last comma and space
+
+				-- send the message three times
+				for i = 1, 3 do
+					local info = LANG.MsgAll("ttt2_hanfei_chat_reveal", { playername = hanfei_players }, MSG_CHAT_WARN)
+				end
+			end
+		end)
+	end)
+
+	-- Remove the player's equipment and weapons when the character changes or respawns
 	function ROLE:RemoveRoleLoadout(ply, isRoleChange)
-        if isRoleChange then
-		    --ply:RemoveEquipmentWeapon("weapon_ttt_kraber")
-			ply:RemoveEquipmentWeapon("weapon_ttt_ak57")
-			ply:RemoveEquipmentWeapon("weapon_ttt_c4")
-            ply:RemoveArmor(GetConVar("ttt_hanfei_armor"):GetInt())
-        end
-    end
+		if isRoleChange then
+			-- Removes the player's equipment and weapons
+			-- Remove AK57 if the player has one
+			if ply:HasWeapon("weapon_ttt_ak57") then
+				ply:RemoveEquipmentWeapon("weapon_ttt_ak57")
+			end
+
+			-- If player has C4, remove C4
+			if ply:HasWeapon("weapon_ttt_c4") then
+				ply:RemoveEquipmentWeapon("weapon_ttt_c4")
+			end
+
+			-- Remove the player's armor
+			ply:RemoveArmor(GetConVar("ttt_hanfei_armor"):GetInt()) -- Note: there is an error in your original code, it should be "ttt_hanfei_armor" not "ttt_hanfei_armor"
+		end
+	end
 end
 
 if CLIENT then
 	function ROLE:AddToSettingsMenu(parent)
-	    local form = vgui.CreateTTT2Form(parent, "header_roles_additional")
-        form:MakeSlider({
-            serverConvar = "ttt_hanfei_armor",
-            label = "label_hanfei_armor",
-            min = 0,
-            max = 500,
-            decimal = 0
-        })
+		local form = vgui.CreateTTT2Form(parent, "header_roles_additional")
 		form:MakeSlider({
-            serverConvar = "ttt_hanfei_hp",
-            label = "label_hanfei_hp",
-            min = 0,
-            max = 500,
-            decimal = 0
-        })		
+			serverConvar = "ttt_hanfei_armor",
+			label = "label_hanfei_armor",
+			min = 0,
+			max = 500,
+			decimal = 0
+		})
 		form:MakeSlider({
-            serverConvar = "ttt_hanfei_exposetime",
-            label = "label_hanfei_exposetime",
-            min = 0,
-            max = 120,
-            decimal = 0
-        })
+			serverConvar = "ttt_hanfei_hp",
+			label = "label_hanfei_hp",
+			min = 0,
+			max = 500,
+			decimal = 0
+		})
+		form:MakeSlider({
+			serverConvar = "ttt_hanfei_exposetime",
+			label = "label_hanfei_exposetime",
+			min = 0,
+			max = 120,
+			decimal = 0
+		})
 	end
 end
