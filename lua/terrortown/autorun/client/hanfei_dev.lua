a2f3edd9668352abf8ecd6968302f9c9f81700b6
波と一緒に漂う <2247564152@qq.com>
2023-10-11 13:01:22 +0800

    Update hanfei_dev.lua

--- lua/terrortown/autorun/client/hanfei_dev.lua
+++ lua/terrortown/autorun/client/hanfei_dev.lua
@@ -5,6 +5,5 @@ if CLIENT then
         AddTTT2AddonDev("76561197990533517") -- sogou
         AddTTT2AddonDev("76561199046379906") -- 愚かなキツネ
         AddTTT2AddonDev("76561198444795757") -- 波と一緒に漂う
-        AddTTT2AddonDev("76561198032479768") -- Zaratusa
     end)
 end
