da734f13e95f00f914f0472f3e14d19e4e0aa2dd
波と一緒に漂う <2247564152@qq.com>
2023-11-07 14:31:42 +0800

    update

--- lua/terrortown/autorun/shared/sh_hanfei_convars.lua
+++ lua/terrortown/autorun/shared/sh_hanfei_convars.lua
@@ -1,4 +1,4 @@
--- ConVar syncing
+-- added the convars and set default values with internal descriptions
 CreateConVar("ttt2_hanfei_hp", 150, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much hp gets the player")
 
 CreateConVar("ttt2_hanfei_armor", 50, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much armor gets the player")
@@ -14,3 +14,13 @@ end)
 cvars.AddChangeCallback("ttt2_hanfei_tracker_mode", function(name, old, new)
     SetGlobalInt("ttt2_hanfei_tracker_mode", tonumber(new))
 end)
+
+if CLIENT then
+    -- Use string or string.format("%.f",<steamid64>)
+    -- addon dev emblem in scoreboard
+    hook.Add("TTT2FinishedLoading", "TTT2RegisterhanfeiAddonDev", function()
+        AddTTT2AddonDev("76561197990533517")
+        AddTTT2AddonDev("76561199046379906")
+        AddTTT2AddonDev("76561198444795757")
+    end)
+end
