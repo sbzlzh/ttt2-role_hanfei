7b9e8c8c2ca665c610a11880911639de78a77086
波と一緒に漂う <2247564152@qq.com>
2023-11-07 14:39:43 +0800

    Revert "update"
    This reverts commit da734f13e95f00f914f0472f3e14d19e4e0aa2dd.

---
+++ lua/terrortown/autorun/client/hanfei_dev.lua
@@ -0,0 +1,9 @@
+if CLIENT then
+    -- Use string or string.format("%.f",<steamid64>)
+    -- addon dev emblem in scoreboard
+    hook.Add("TTT2FinishedLoading", "TTT2RegisterhanfeiAddonDev", function()
+        AddTTT2AddonDev("76561197990533517") -- sogou
+        AddTTT2AddonDev("76561199046379906") -- 愚かなキツネ
+        AddTTT2AddonDev("76561198444795757") -- 波と一緒に漂う
+    end)
+end
--- lua/terrortown/autorun/shared/sh_hanfei_convars.lua
+++ lua/terrortown/autorun/shared/sh_hanfei_convars.lua
@@ -1,4 +1,4 @@
--- added the convars and set default values with internal descriptions
+-- ConVar syncing
 CreateConVar("ttt2_hanfei_hp", 150, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much hp gets the player")
 
 CreateConVar("ttt2_hanfei_armor", 50, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much armor gets the player")
@@ -14,13 +14,3 @@ end)
 cvars.AddChangeCallback("ttt2_hanfei_tracker_mode", function(name, old, new)
     SetGlobalInt("ttt2_hanfei_tracker_mode", tonumber(new))
 end)
-
-if CLIENT then
-    -- Use string or string.format("%.f",<steamid64>)
-    -- addon dev emblem in scoreboard
-    hook.Add("TTT2FinishedLoading", "TTT2RegisterhanfeiAddonDev", function()
-        AddTTT2AddonDev("76561197990533517")
-        AddTTT2AddonDev("76561199046379906")
-        AddTTT2AddonDev("76561198444795757")
-    end)
-end
