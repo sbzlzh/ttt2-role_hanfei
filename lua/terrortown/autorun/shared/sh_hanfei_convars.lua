b03c0199263b0aaeca9801904bca194b70f923c4
波と一緒に漂う <2247564152@qq.com>
2023-11-11 02:02:33 +0800

    Update sh_hanfei_convars.lua

--- lua/terrortown/autorun/shared/sh_hanfei_convars.lua
+++ lua/terrortown/autorun/shared/sh_hanfei_convars.lua
@@ -1,11 +1,11 @@
 -- added the convars and set default values with internal descriptions
-CreateConVar("ttt2_hanfei_hp", 150, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much hp gets the player")
+CreateConVar("ttt2_hanfei_hp", 150, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much hp gets the player")
 
-CreateConVar("ttt2_hanfei_armor", 50, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much armor gets the player")
+CreateConVar("ttt2_hanfei_armor", 50, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much armor gets the player")
 
-CreateConVar("ttt2_hanfei_exposetime", 60, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "How much exposetime gets the player")
+CreateConVar("ttt2_hanfei_exposetime", 60, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "How much exposetime gets the player")
 
-CreateConVar("ttt2_hanfei_tracker_mode", 0, { FCVAR_NOTIFY, FCVAR_ARCHIVE }, "What type of tracking item should the HanFei start with?")
+CreateConVar("ttt2_hanfei_tracker_mode", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "What type of tracking item should the HanFei start with?")
 
 hook.Add("TTT2SyncGlobals", "AddHanFeiGlobals", function()
     SetGlobalInt("ttt2_hanfei_tracker_mode", GetConVar("ttt2_hanfei_tracker_mode"):GetInt())
